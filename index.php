6bd4050d3cf0027c18367b287c104597ed42826a
Gildo Costa Francisco Chauze <89664210+gildoch@users.noreply.github.com>
2022-02-16 09:35:07 +0200

    Criacao da tela de visualizacao de notas e adicao de butoes para alteracao do conteudo e remocao de conteudo

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include 'dbConnect.php'; /* importamos a conexao */ ?>
+<?php 
+    include 'dbConnect.php'; /* importamos a conexao */
+    if(isset($_POST['noteTitle'])){
+    $ntNumber = rand(4,100);
+    $ntTitle = $_POST['noteTitle'];
+    $ntContent = $_POST['noteContent'];
+    $stmt = $conn -> prepare("insert into notes (noteNumber, noteTitle, noteContent) values (?,?,?)
+    ");
+    $stmt -> bindParam(1, $ntNumber);
+    $stmt -> bindParam(2,$ntTitle);
+    $stmt -> bindParam(3,$ntContent);
+    if($stmt->execute()){
+        echo "Nota gravada com sucesso";
+    }
+    } else {
+        
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,6 +23,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="style.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -14,25 +32,24 @@
 <body>
     
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <a class="navbar-brand" href="#">Title</a>
+        <a class="navbar-brand" href="#">My Notes</a>
         <ul class="nav navbar-nav">
             <li class="active">
-                <a href="#">Home</a>
+                <a href="#">Add Note</a>
             </li>
             <li>
-                <a href="#">Link</a>
+                <a href="viewNotes.php">View Notes</a>
             </li>
         </ul>
-    </nav>
-    
+    </nav>  
     
     <div class="row">
         <div class="col-md"></div>
         <div class="col-md-6">
             
-            <form action="" method="POST" role="form">
-                 <legend>My Notes</legend>
-            
+            <form action="index.php" method="POST" role="form">
+                 <legend>Add Notes</legend>
+            <hr>
                 <div class="form-group">
                     <input type="text" class="form-control" id="noteTitle" name="noteTitle" placeholder="Note Title">
                     <textarea name="noteContent" id="noteContent" cols="30" rows="10" placeholder="Note Content"></textarea>
--- viewNotes.php
+++ viewNotes.php
@@ -1,4 +1,9 @@
-<?php include 'dbConnect.php'; /* importamos a conexao */ ?>
+<?php 
+    include 'dbConnect.php'; /* importamos a conexao */ 
+    $stmt = $conn->prepare('select * from notes'); /*preparamos a query antes de exeutar, isso evite sql injection*/
+	$stmt->execute(); /*execitamos a query*/
+	$notas=$stmt->fetchAll(PDO::FETCH_OBJ); /*extraimos o resultset para array de objectos*/
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,19 +11,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>View Notes</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <a class="navbar-brand" href="#">My Notes</a>
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="index.php">Add Note</a>
+            </li>
+            <li>
+                <a href="#">View Notes</a>
+            </li>
+        </ul>
+    </nav>>
+
     <div class="row">
         <div class="col-md"></div>
         <div class="col-md-6">
+            <h1 class="myNotesTitle">My Notes</h1>
+            
+            <table class="table table-stripped table-hover">
+                <thead>
+                    <tr>
+                        <th>Note Title</th>
+                        <th>Note Content</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($notas as $a){ ?>
+			        <tr>
+				         <td><?php echo $a->noteTitle; ?></td>
+                         <td><?php echo $a->notecontent; ?></td>
+                         <td><button type="submit" class="btn btn-primary">EDIT</button></td>
+                         <td><button type="submit" class="btn btn-primary">DELETE</button></td>
+			        </tr>
+		            <?php } ?>
+                </tbody>
+            </table>
             
         </div>
         <div class="col-md"></div>
-        
     </div>
 </body>
 </html>
